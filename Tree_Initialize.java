16cdb481ed480f7cee6a4e6973c936ecd54d0914
Ethan Kamolsiri <144487144+Ethan-Kamolsiri@users.noreply.github.com>
2026-03-10 20:08:49 -0500

    Update Tree_Initialize.java
    updated

--- Tree_Initialize.java
+++ Tree_Initialize.java
@@ -6,16 +6,55 @@ import java.nio.file.Paths;
 
 
 class Tree_Initialize{
-    public static void main(String [] args){
-        int numFiles = 0; //25
-        int numLines = 0; //500
-        BufferedReader br = new BufferedReader(new FileReader(args[0]));
-        String line;
-        while((line = br.readLine()) != null){
-            numLines++;
-            while(numLines < 500){
-                line = line.toLowerCase();
-            }
+    public static final int MAX_LINES = 25000;
+    public static final int MAX_FILES = 25;
+    public static HashMap<String, HashMap<String, Integer>> TF;  
+    public static Path WetDir;
+    public static Path ProjectDir;     
+    public static void main(String [] args) throws Exception{
+
+       String wetDirName = args[0];
+       Path WetDir = Paths.get(wetDirName);
+       Path ProjectDir = WetDir.getParent();
+    }
+
+    public static void readDirectory(String wetDir)throws Exception{
+        File folder = new File(wetDir);
+        File[] files = folder.listFiles();
+        for(File f : files){
+            Pass_1_WET(f.getName());
         }
     }
+
+    public static String[] Pass_1_WET(String f)throws Exception{        
+        int numFiles = 0; //25
+        int numLines = 0; //500
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        Path path = Paths.get();
+        try {
+            // This creates the folder and any missing parents
+            Files.createDirectories(path);
+            System.out.println("Directory created successfully!");
+        } catch (IOException e) {
+            System.err.println("Failed to create directory: " + e.getMessage());
+        }
+        String line;
+        while((line = br.readLine()) != null){
+            String[] tokens = line.split("\\s+");
+            for(String token : tokens){
+                boolean isEnglish = true;
+                for(int i = 0; i < token.length(); i++){
+                    if((int)token.charAt(i) < 33 || (int)token.charAt(i) > 126){
+                        isEnglish = false;
+                        break;
+                    }
+                }
+            }
+
+            
+        }
+    }
+
 }
+
+
